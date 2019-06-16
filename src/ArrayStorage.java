743514c457d07fa26605ca90ba01365e5372f0b2
Andrey <shutlegend@yandex.ru>
2019-06-16 21:43:15 +0300

    реализация методов

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,54 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i<storage.length; i++){
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length; i++){
+            if (r != null){
+                storage[i] = r;
+            }
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume resume = null;
+        for (int i = 0; i < storage.length; i++){
+            if (storage[i].equals(uuid)){
+                resume = storage[i];
+            }
+        }
+        return resume;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < storage.length; i++){
+            if (storage[i].equals(uuid)){
+                storage[i] = null;
+                for (int j = i; j < storage.length; j++){
+                    storage[j] = storage[j+1];
+                }
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return storage;
     }
 
     int size() {
-        return 0;
+        int count = 0;
+        for (int i = 0; i < storage.length; i++){
+            if (storage[i] != null){
+                count++;
+            }
+        }
+        return count;
     }
 }
